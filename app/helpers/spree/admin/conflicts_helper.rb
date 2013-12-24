b1ebc213bae6c2f66658299bc71548f1563355d8
GarPit <garik.piton@gmail.com>
2013-12-24 16:09:41 +0400

    update product action is better to place first

--- app/helpers/spree/admin/conflicts_helper.rb
+++ app/helpers/spree/admin/conflicts_helper.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     module ConflictsHelper
       def conflict_solve_actions
-        [:create_new_product, :update_product].map do |action|
+        [:update_product, :create_new_product].map do |action|
           [Spree.t(action), action]
         end
       end
