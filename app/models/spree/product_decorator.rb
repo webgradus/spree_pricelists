2746151af48fbc4bb21a5340b445cd2302ad02b9
Pavel <buurzx@gmail.com>
2015-04-21 14:31:33 +0500

    fixes

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -8,7 +8,7 @@ Spree::Product.class_eval do
   before_update :update_synonim, if: lambda {|product| product.name_changed? }
   after_create :add_synonim
 
-  scope :find_by_synonim, lambda { |name| includes(:product_synonims).where("spree_product_synonims.name = ?", name).limit(1)}
+  scope :find_by_synonim, lambda { |name| joins(:product_synonims).where("spree_product_synonims.name = ?", name).limit(1)}
 
   pg_search_scope :name_matching,
     associated_against: {product_synonims: :name},
--- lib/spree/importers/base_importer.rb
+++ lib/spree/importers/base_importer.rb
@@ -5,22 +5,28 @@ class Spree::Importers::BaseImporter
 
   def self.parse(pricelist_id, file, starting_row)
     tmp_file_path = create_tmp_csv_file(file)
+    # byebug
     delayed_start(pricelist_id, tmp_file_path, starting_row)
   end
 
   def initialize(pricelist, file_path, begin_point)
+    # byebug
     @log = Logger.new(STDOUT)
     @pricelist = pricelist
     @starting_row = begin_point
     @file_path = file_path
-    @taxonomy = Spree::Taxonomy.where(:name => @pricelist.name).first_or_create!
-    @taxon = @taxonomy.root
+    @taxon = Spree::Taxon.find_or_create_by(:name => @pricelist.name)
+    @taxonomy = @taxon.taxonomy
+    # @taxon = @taxonomy.root
     @parsed_products = []
+    @taxon_root = Spree::Taxon.find_by_name(@pricelist.name)
+    
   end
 
   def import
     log.info("#"*25 << "Start #{Time.now.to_s}" << "#"*25)
     current_row = 1
+    # byebug
     CSV.foreach(file_path, col_sep: ';') do |row|
       parse_csv_row(row) if current_row >= starting_row.to_i
       current_row += 1
@@ -31,8 +37,13 @@ class Spree::Importers::BaseImporter
 
   protected
 
+  def self.find_all_by_name(name)
+    find(:all, :conditions => ["LOWER(name) = ?", name.downcase])
+  end
+
   def update_missed_products
     # we need to delay it cause we need to make sure all DataFactory workers were done
+    # byebug
     Spree::UpdateMissedProductsWorker.perform_in(10.minutes, pricelist.id, @parsed_products)
   end
 
@@ -53,6 +64,7 @@ class Spree::Importers::BaseImporter
               #available_on: ::Time.now
     }
     if row_is_taxon?(row)
+      # byebug
       create_taxon(row)
       @up = false
     else
@@ -80,29 +92,32 @@ class Spree::Importers::BaseImporter
   end
 
   def create_taxon(row)
-    if @up
-      parent = taxon.parent
-      parent = parent || taxonomy.root
-    else
-      parent = taxon
-      if taxon.products.count(:id) == 0 && taxon.parent_id != taxonomy.root.id && !taxon.parent_id.nil?
-        log.info("Переназначаем родителя #{taxon.name}")
-        taxon.parent = taxonomy.root
-        taxon.permalink=''
-        taxon.set_permalink
-        taxon.save
-      end
-    end
-    @taxon = save_taxon(parent, row)
+    # if @up
+    #   parent = taxon.parent
+    #   parent = parent || @taxon_root
+    # else
+    #   parent = taxon
+    #   if taxon.products.count(:id) == 0 && taxon.parent_id != taxonomy.root.id && !taxon.parent_id.nil?
+    #     log.info("Переназначаем родителя #{taxon.name}")
+    #     taxon.parent = taxonomy.root
+    #     taxon.permalink=''
+    #     taxon.set_permalink
+    #     taxon.save
+    #   end
+    # end
+    @taxon = save_taxon(@taxon_root, row)
   end
 
   def save_taxon(parent, row)
     log.info("Переназначаем родителя #{taxon.name}")
-    tax = Spree::Taxon.where(:name=>row.compact.first.to_s,:taxonomy_id => taxonomy.id,:parent_id=>parent.id).first
+    # byebug
+    # tax = Spree::Taxon.where(:name=>row.compact.first.to_s,:taxonomy_id => taxonomy.id,:parent_id=>parent.id).first
+    tax = Spree::Taxon.where("name ILIKE ?", row.compact.first.to_s).map{|x| x if x.parent_id == parent.id or x.parent.parent_id == parent.id}.compact.first
     if tax.nil?
-      tax = Spree::Taxon.new(:name=>row.compact.first.to_s)
+      tax = Spree::Taxon.new(:name=>row.compact.first.mb_chars.capitalize.to_s)
       tax.taxonomy = taxonomy
       tax.parent=parent
+      # byebug
       tax.save
     end
     return tax
@@ -116,8 +131,12 @@ class Spree::Importers::BaseImporter
     csv_tmp_file = "#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S")}.csv"
     tmp_file = "#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S")}.xlsx"
     File.open(tmp_file, 'wb') do |f|
-      f.write  file.read
+      f.write file.read
     end
+    # File.open(tmp_file, 'wb') do |f|
+    #   f.write  file.read
+    # end
+    # byebug
     unless system("python lib/xlsx2csv.py -i -d '\;' #{tmp_file} #{csv_tmp_file}")
       raise "Invalid XLSX format ERROR!"
     end
