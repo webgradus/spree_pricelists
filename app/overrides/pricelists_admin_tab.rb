cb1c24979f7dce144c3fbc470f5bd860ea79d4b4
IvanZabrodin <xvanx84@gmail.com>
2016-08-10 14:15:30 +0300

    Update pricelists_admin_tab.rb

--- app/overrides/pricelists_admin_tab.rb
+++ app/overrides/pricelists_admin_tab.rb
@@ -1,5 +1,9 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "pricelists_admin_tab",
-                     :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:new_import, :url => new_import_admin_pricelists_path, :icon => 'icon-magic') %>",
-                     :disabled => false)
+                    :name => "pricelists_admin_tab",
+                    :insert_bottom => "[data-hook='admin_tabs']",
+                    :text => "
+                              <ul class='nav nav-sidebar'>
+                                <%= tab(:new_import, :url => new_import_admin_pricelists_path, :icon => 'file') %>
+                              </ul>
+                             ",
+                    :disabled => false)
