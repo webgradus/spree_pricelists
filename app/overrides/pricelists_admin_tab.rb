fe9b94a254dd7582a86abfdadd53f3862d82a92c
EvanBrightside <xvanx84@gmail.com>
2016-08-14 20:44:17 +0300

    Fix overrides and views for new version of spree

--- app/overrides/pricelists_admin_tab.rb
+++ app/overrides/pricelists_admin_tab.rb
@@ -1,9 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                     :name => "pricelists_admin_tab",
-                    :insert_bottom => "[data-hook='admin_tabs']",
-                    :text => "
-                              <ul class='nav nav-sidebar'>
-                                <%= tab(:new_import, :url => new_import_admin_pricelists_path, :icon => 'file') %>
-                              </ul>
-                             ",
+                    :insert_bottom => "#main-sidebar",
+                    :partial => 'spree/admin/shared/pricelists_sidebar_menu',
                     :disabled => false)
