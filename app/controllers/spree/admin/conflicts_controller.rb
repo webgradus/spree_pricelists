3cd0bebd42d57a0ace5d3bf5cdf14f770725be6c
GarPit <garik.piton@gmail.com>
2016-09-23 19:11:38 +0300

    order conflicts by created_at desc

--- app/controllers/spree/admin/conflicts_controller.rb
+++ app/controllers/spree/admin/conflicts_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class ConflictsController < ResourceController
       def index
-        @conflicts = Spree::Conflict.page(params[:page]).per(5)
+        @conflicts = Spree::Conflict.order('created_at desc').page(params[:page]).per(5)
       end
 
       def update
