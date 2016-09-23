5932d6667160aa7c8c2e84b529afd040534f01b1
GarPit <garik.piton@gmail.com>
2016-09-23 16:21:21 +0300

    update html and support spree 3+

--- app/controllers/spree/admin/product_synonims_controller.rb
+++ app/controllers/spree/admin/product_synonims_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   module Admin
     class ProductSynonimsController < ResourceController
-      belongs_to 'spree/product', :find_by => :permalink
+      belongs_to 'spree/product', :find_by => :slug
     end
   end
 end
--- app/overrides/add_product_synonims_tab.rb
+++ app/overrides/add_product_synonims_tab.rb
@@ -2,8 +2,8 @@ Deface::Override.new(:virtual_path => 'spree/admin/shared/_product_tabs',
   :name => 'add_product_synonims_tab_to_products',
   :insert_bottom => "[data-hook='admin_product_tabs']",
   :text => "
-      <%= content_tag :li, :class => ('active' if current == 'Product Synonims') do %>
-        <%= link_to_with_icon 'icon-copy', Spree.t(:product_synonims), admin_product_product_synonims_url(@product) %>
+      <%= content_tag :li, :class => ('active' if current == :product_synonims) do %>
+        <%= link_to_with_icon 'clone', Spree.t(:product_synonims), admin_product_product_synonims_url(@product) %>
       <% end if can?(:admin, Spree::Product) %>
   "
 )
